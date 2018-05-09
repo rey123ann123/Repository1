4c1bec855e46c2ba8dc0bb7989711377b7bb9047
rcheca@deloitte <rcheca@deloitte>
2018-05-10 00:40:40 +1000

    sample commit 1

---
+++ repo_settings/sample_repo.sh
@@ -0,0 +1,26 @@
+# Set your values below
+
+
+# It is extraordinarily dangerous for your repo to do a mistake here.
+# More often a mess with settings from different repos hurts you here.
+
+
+# A folder-name where will be stored local repos for syncing.
+project_folder=github
+
+
+# A conventional prefix for refs for repo 1.
+prefix_1=repo_1-
+url_1=https://github.com/rey123ann123/Repository1.git
+
+
+# A conventional prefix for refs for repo 1.
+prefix_2=repo_2-
+url_2=https://github.com/rey123ann123/Repository2.git
+
+
+# Some branch that must exist on both sides to allow any deletion of refs.
+must_exist_branch=${prefix_1}production
+
+
+
